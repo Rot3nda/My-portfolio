f54e11f9f2204d10015e6a97dfcffc8d001e04d4
Rotenda Ramugondo <rotendaramugondo@Rotendas-MacBook-Pro.local>
2025-02-16 15:52:42 +0200

    files upload

---
+++ p.js
@@ -0,0 +1,9 @@
+function navHover()
+{
+document.getElementById("homenav").style.borderBottom="0px";
+}
+
+function navLeave()
+{
+document.getElementById("homenav").style.borderBottom="2px solid rgb(141, 42, 239)";
+}
