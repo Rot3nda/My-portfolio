c44e5c69f50c257de590330e4da28fbf2b064af8
Rotenda Ramugondo <rotendaramugondo@Rotendas-MacBook-Pro.local>
2025-02-16 23:50:57 +0200

    up

--- p.js
+++ p.js
@@ -7,3 +7,21 @@ function navLeave()
 {
 document.getElementById("homenav").style.borderBottom="2px solid rgb(141, 42, 239)";
 }
+
+function sidebarPop(){
+   document.getElementById("sidebar").style.display="block";
+   document.getElementById("back").style.display="block";
+   document.getElementById("menu").style.display="none";
+   document.getElementById("line1").style.visibility="hidden";
+   document.getElementById("line2").style.visibility="hidden";
+   document.getElementById("line3").style.visibility="hidden";
+}
+
+function sidebarOff(){
+    document.getElementById("sidebar").style.display="none";
+    document.getElementById("back").style.display="none";
+    document.getElementById("menu").style.display="block";
+    document.getElementById("line1").style.visibility="visible";
+   document.getElementById("line2").style.visibility="visible";
+   document.getElementById("line3").style.visibility="visible";
+}
